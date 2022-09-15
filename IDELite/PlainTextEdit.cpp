59cd50d1219405a11bf5331a339dc5dbed0d935f
Jaward Sesay <jkef96@icloud.com>
2022-09-15 23:38:43 +0800

    Update PlainTextEdit.cpp

--- IDELite/PlainTextEdit.cpp
+++ IDELite/PlainTextEdit.cpp
@@ -3,9 +3,7 @@
 PlainTextEdit::PlainTextEdit(QPlainTextEdit *parent) : QPlainTextEdit(parent)
 {
     QFont codeFont("Consolas", 12, 2);
-//    this->setStyleSheet("QPlainTextEdit{background-color:rgb(255,255,255);border:none; border-radius:20px;color:rgb(93, 93, 94);"
-//                            "selection-background-color:rgb(228, 228, 228);selection-color:rgb(147, 115, 238);"
-//                            "padding: 20px;}");
+
     this->setFont(codeFont);
     this->setLineWrapMode(LineWrapMode::NoWrap);
 
